6c4ef17bdd88fba346d8f392d631b9370e0632fa
Anne Blankert <anne.blankert@geodan.nl>
2023-08-02 12:46:15 +0200

    add user inputs

---
+++ colors.js
@@ -0,0 +1,52 @@
+export const colors = [
+    "#000000", // Black
+    "#FFFFFF", // White
+    "#FF0000", // Red
+    "#00FF00", // Lime
+    "#0000FF", // Blue
+    "#FFFF00", // Yellow
+    "#00FFFF", // Aqua
+    "#FF00FF", // Fuchsia
+    "#800000", // Maroon
+    "#008000", // Green
+    "#000080", // Navy
+    "#808000", // Olive
+    "#800080", // Purple
+    "#008080", // Teal
+    "#C0C0C0", // Silver
+    "#808080", // Gray
+    "#999999", // Dark Gray
+    "#333333", // Very Dark Gray
+    "#FF9999", // Light Red
+    "#99FF99", // Light Green
+    "#9999FF", // Light Blue
+    "#FFFF99", // Light Yellow
+    "#99FFFF", // Light Aqua
+    "#FF99FF", // Light Fuchsia
+    "#CC0000", // Dark Red
+    "#00CC00", // Dark Green
+    "#0000CC", // Dark Blue
+    "#CCCC00", // Dark Yellow
+    "#00CCCC", // Dark Aqua
+    "#CC00CC", // Dark Fuchsia
+    "#66B032", // Apple Green
+    "#FF7043", // Burnt Orange
+    "#8A2BE2", // Blue Violet
+    "#7FFF00", // Chartreuse
+    "#D2691E", // Chocolate
+    "#DC143C", // Crimson
+    "#FF4500", // Orange Red
+    "#2E8B57", // Sea Green
+    "#ADFF2F", // Green Yellow
+    "#FF69B4", // Hot Pink
+    "#4B0082", // Indigo
+    "#FFFFF0", // Ivory
+    "#F0E68C", // Khaki
+    "#E6E6FA", // Lavender
+    "#FFF0F5", // Lavender Blush
+    "#BDB76B", // Dark Khaki
+    "#8B008B", // Dark Magenta
+    "#556B2F", // Dark Olive Green
+    "#FF8C00", // Dark Orange
+    "#9932CC"  // Dark Orchid
+  ];
--- index.js
+++ index.js
@@ -1,109 +1,178 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import {colors} from './colors.js';
 
 const objLoader = new OBJLoader();
 const mtlLoader = new MTLLoader();
 
-const scene = new THREE.Scene();
+
 
 const baseName = 'maquette_krimpen_cc_21-537684-346784';
+
+const scaleElement = document.querySelector('#scale');
+const maxAngleElement = document.querySelector('#maxangle');
+const showTrianglesElement = document.querySelector('#showtriangles');
+const showPolygonsElement = document.querySelector('#showpolygons');
+const canvasElement = document.querySelector('#canvas');
+
+let scale = scaleElement.value;
+let angle = maxAngleElement.value;
+let showtriangles = showTrianglesElement.checked;
+let showpolygons = showPolygonsElement.checked;
+scaleElement.addEventListener('change', e => {scale = e.target.value; drawPage()});
+maxAngleElement.addEventListener('change', e => {angle = e.target.value; drawPage()});
+showTrianglesElement.addEventListener('change', e => {showtriangles = e.target.checked; drawPage()});
+showPolygonsElement.addEventListener('change', e => {showpolygons = e.target.checked; drawPage()});
+canvasElement.addEventListener('click', e => {
+    // get coordinates of click
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const w = canvasElement.width;
+    const h = canvasElement.height;
+    const uv = new THREE.Vector2(x / w, 1 - y / h);
+    const triangle = findTriangle(uv);
+    updateStatus(`Triangle: ${triangle}`);
+});
+
+function pointInTriangle(p, a, b, c) {
+    // Compute vectors        
+    let v0 = c.clone().sub(a);
+    let v1 = b.clone().sub(a);
+    let v2 = p.clone().sub(a);
+
+    // Compute dot products
+    let dot00 = v0.dot(v0);
+    let dot01 = v0.dot(v1);
+    let dot02 = v0.dot(v2);
+    let dot11 = v1.dot(v1);
+    let dot12 = v1.dot(v2);
+
+    // Compute barycentric coordinates
+    let invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+    let u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+    let v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+    // Check if point is in triangle
+    return (u >= 0) && (v >= 0) && (u + v < 1);
+}
+
+function findTriangle(uv) {
+    for (let i = 0; i < textures.length; i++) {
+        // texture is an array of 3 uv coordinates
+        const texture = textures[i];
+        const uvA = texture[0];
+        const uvB = texture[1];
+        const uvC = texture[2];
+        // check if uv is inside triangle
+        if (pointInTriangle(uv, uvA, uvB, uvC)) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 let triangles = [];
 let textures = [];
 
-
-mtlLoader.load(`data/${baseName}.mtl`, materials => {
-    materials.preload();
-    objLoader.setMaterials(materials);
-    objLoader.load(`data/${baseName}.obj`, object => {
-        scene.add(object);
-        scene.traverse(child => {
-            if (child instanceof THREE.Mesh) {
-                // Access to vertices
-                const vertices = child.geometry.attributes.position.array;
-                const uvArray = child.geometry.attributes.uv.array // grab the uv array from the geometry
-                triangles = [];
+function drawPage() {
+    triangles = [];
+    textures = [];
+    const scene = new THREE.Scene();
+    document.querySelector('#info').innerHTML = `Loading...`;
+    mtlLoader.load(`data/${baseName}.mtl`, materials => {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load(`data/${baseName}.obj`, object => {
+            scene.add(object);
+            scene.traverse(child => {
+                if (child instanceof THREE.Mesh) {
+                    // Access to vertices
+                    const vertices = child.geometry.attributes.position.array;
+                    const uvArray = child.geometry.attributes.uv.array // grab the uv array from the geometry
+                    triangles = [];
+                    
+                    if (child.geometry.index !== null) {
+                        // Indexed BufferGeometry
+                        const indices = child.geometry.index.array;
                 
-                if (child.geometry.index !== null) {
-                    // Indexed BufferGeometry
-                    const indices = child.geometry.index.array;
-            
-                    for (let i = 0; i < indices.length; i += 3) {
-                        let aIndex = indices[i] * 3;
-                        let bIndex = indices[i+1] * 3;
-                        let cIndex = indices[i+2] * 3;
+                        for (let i = 0; i < indices.length; i += 3) {
+                            let aIndex = indices[i] * 3;
+                            let bIndex = indices[i+1] * 3;
+                            let cIndex = indices[i+2] * 3;
 
-                        // Create THREE.Vector3 vertices
-                        let a = new THREE.Vector3(vertices[aIndex], vertices[aIndex+1], vertices[aIndex+2]);
-                        let b = new THREE.Vector3(vertices[bIndex], vertices[bIndex+1], vertices[bIndex+2]);
-                        let c = new THREE.Vector3(vertices[cIndex], vertices[cIndex+1], vertices[cIndex+2]);
+                            // Create THREE.Vector3 vertices
+                            let a = new THREE.Vector3(vertices[aIndex], vertices[aIndex+1], vertices[aIndex+2]);
+                            let b = new THREE.Vector3(vertices[bIndex], vertices[bIndex+1], vertices[bIndex+2]);
+                            let c = new THREE.Vector3(vertices[cIndex], vertices[cIndex+1], vertices[cIndex+2]);
 
-                        // If the mesh has any transformations applied, you'll need to apply them to the vertices as well.
-                        if (child.matrixWorld) {
-                            a.applyMatrix4(child.matrixWorld);
-                            b.applyMatrix4(child.matrixWorld);
-                            c.applyMatrix4(child.matrixWorld);
+                            // If the mesh has any transformations applied, you'll need to apply them to the vertices as well.
+                            if (child.matrixWorld) {
+                                a.applyMatrix4(child.matrixWorld);
+                                b.applyMatrix4(child.matrixWorld);
+                                c.applyMatrix4(child.matrixWorld);
+                            }
+
+                            // Create a THREE.Triangle from the vertices
+                            let triangle = new THREE.Triangle(a, b, c);
+
+                            // Add the triangle to the triangles array
+                            triangles.push(triangle);
+
+                            // Get the indices of the uvs of the face
+                            let aUVIndex = indices[i] * 2;
+                            let bUVIndex = indices[i+1] * 2;
+                            let cUVIndex = indices[i+2] * 2;
+
+                            if (uvArray) {
+                                // Create UV coordinates
+                                let aUV = new THREE.Vector2(uvArray[aUVIndex], uvArray[aUVIndex + 1]);
+                                let bUV = new THREE.Vector2(uvArray[bUVIndex], uvArray[bUVIndex + 1]);
+                                let cUV = new THREE.Vector2(uvArray[cUVIndex], uvArray[cUVIndex + 1]);
+
+                                // Add the uv coordinates to the uvs array
+                                textures.push([aUV, bUV, cUV]);
+                            }
                         }
+                    } else {
+                        // Non-indexed BufferGeometry
+                        for (let i = 0; i < vertices.length; i += 9) {
+                            // Create THREE.Vector3 vertices directly from the positions array
+                            let a = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
+                            let b = new THREE.Vector3(vertices[i+3], vertices[i+4], vertices[i+5]);
+                            let c = new THREE.Vector3(vertices[i+6], vertices[i+7], vertices[i+8]);
 
-                        // Create a THREE.Triangle from the vertices
-                        let triangle = new THREE.Triangle(a, b, c);
+                            // If the mesh has any transformations applied, you'll need to apply them to the vertices as well.
+                            if (child.matrixWorld) {
+                                a.applyMatrix4(child.matrixWorld);
+                                b.applyMatrix4(child.matrixWorld);
+                                c.applyMatrix4(child.matrixWorld);
+                            }
 
-                        // Add the triangle to the triangles array
-                        triangles.push(triangle);
+                            // Create a THREE.Triangle from the vertices
+                            let triangle = new THREE.Triangle(a, b, c);
 
-                        // Get the indices of the uvs of the face
-                        let aUVIndex = indices[i] * 2;
-                        let bUVIndex = indices[i+1] * 2;
-                        let cUVIndex = indices[i+2] * 2;
+                            // Add the triangle to the triangles array
+                            triangles.push(triangle);
 
-                        if (uvArray) {
-                            // Create UV coordinates
-                            let aUV = new THREE.Vector2(uvArray[aUVIndex], uvArray[aUVIndex + 1]);
-                            let bUV = new THREE.Vector2(uvArray[bUVIndex], uvArray[bUVIndex + 1]);
-                            let cUV = new THREE.Vector2(uvArray[cUVIndex], uvArray[cUVIndex + 1]);
+                            if (uvArray) {
+                                // Get the uvs for the face
+                                let aUV = new THREE.Vector2(uvArray[i/3*2], uvArray[i/3*2 + 1]);
+                                let bUV = new THREE.Vector2(uvArray[i/3*2 + 2], uvArray[i/3*2 + 3]);
+                                let cUV = new THREE.Vector2(uvArray[i/3*2 + 4], uvArray[i/3*2 + 5]);
 
-                            // Add the uv coordinates to the uvs array
-                            textures.push([aUV, bUV, cUV]);
-                        }
-                    }
-                } else {
-                    // Non-indexed BufferGeometry
-                    for (let i = 0; i < vertices.length; i += 9) {
-                        // Create THREE.Vector3 vertices directly from the positions array
-                        let a = new THREE.Vector3(vertices[i], vertices[i+1], vertices[i+2]);
-                        let b = new THREE.Vector3(vertices[i+3], vertices[i+4], vertices[i+5]);
-                        let c = new THREE.Vector3(vertices[i+6], vertices[i+7], vertices[i+8]);
-
-                        // If the mesh has any transformations applied, you'll need to apply them to the vertices as well.
-                        if (child.matrixWorld) {
-                            a.applyMatrix4(child.matrixWorld);
-                            b.applyMatrix4(child.matrixWorld);
-                            c.applyMatrix4(child.matrixWorld);
-                        }
-
-                        // Create a THREE.Triangle from the vertices
-                        let triangle = new THREE.Triangle(a, b, c);
-
-                        // Add the triangle to the triangles array
-                        triangles.push(triangle);
-
-                        if (uvArray) {
-                            // Get the uvs for the face
-                            let aUV = new THREE.Vector2(uvArray[i/3*2], uvArray[i/3*2 + 1]);
-                            let bUV = new THREE.Vector2(uvArray[i/3*2 + 2], uvArray[i/3*2 + 3]);
-                            let cUV = new THREE.Vector2(uvArray[i/3*2 + 4], uvArray[i/3*2 + 5]);
-
-                            // Add the uv coordinates to the uvs array
-                            textures.push([aUV, bUV, cUV]);
+                                // Add the uv coordinates to the uvs array
+                                textures.push([aUV, bUV, cUV]);
+                            }
                         }
                     }
+                    clusterize(triangles);
                 }
-                clusterize(triangles);
-            }
+            });
         });
     });
-});
+}
 
 function intersect(a, b) {
     let setB = new Set(b);
@@ -193,7 +262,7 @@ function groupByAdjencyAndNormals(adjacencyMap) {
     });
     
     // The threshold angle in degrees
-    let angleThreshold = 10; 
+    let angleThreshold = angle; 
     
     // Convert the threshold to radians since THREE.js uses radians
     let angleThresholdRadians = angleThreshold * Math.PI / 180;
@@ -220,10 +289,10 @@ function groupByAdjencyAndNormals(adjacencyMap) {
     
     for (let [triangleIndex, adjacentTriangles] of adjacencyMap) {
         if (!visited.has(triangleIndex)) {
-        let component = new Set();
-        dfs(triangleIndex, component);
-        components.push(Array.from(component)); // Convert set to array for simplicity
-        component.forEach(node => visited.add(node));
+            let component = new Set();
+            dfs(triangleIndex, component);
+            components.push(Array.from(component)); // Convert set to array for simplicity
+            component.forEach(node => visited.add(node));
         }
     }
     return components;
@@ -239,6 +308,9 @@ function buildPolygons(connectedComponents) {
         if (triangles.length === 0) {
             continue;
         }
+        /*if (triangles.length !== 2) {
+            continue;
+        }*/
 
         // Start with one triangle
         let polygon = [...textures[triangles[0]]].map(vertex => JSON.stringify(vertex));
@@ -255,24 +327,17 @@ function buildPolygons(connectedComponents) {
             if (sharedVertices.length === 2) {
                 // This triangle shares an edge with the polygon
         
-                // Find the index of the first shared vertex in the polygon
+                // Find the indices of the shared vertices in the polygon
                 let sharedIndex1 = polygon.indexOf(sharedVertices[0]);
-        
-                // Check the winding order
-                if (JSON.stringify(polygon[(sharedIndex1 + 1) % polygon.length]) === sharedVertices[1]) {
-                    // The shared edge is in the same order in both the triangle and the polygon
-        
-                    // Insert the non-shared vertex between the shared vertices
-                    let nonSharedVertex = triangleVertices.find(vertex => !sharedVertices.includes(vertex));
-                    polygon.splice(sharedIndex1 + 1, 0, nonSharedVertex);
-                } else {
-                    // The shared edge is in the opposite order in the triangle and the polygon
-        
-                    // Insert the non-shared vertex outside the shared vertices
-                    let nonSharedVertex = triangleVertices.find(vertex => !sharedVertices.includes(vertex));
+                let sharedIndex2 = polygon.indexOf(sharedVertices[1]);
+
+                // add remaining vertex in between sharedIndex1 and sharedIndex2
+                let nonSharedVertex = triangleVertices.find(vertex => !sharedVertices.includes(vertex));
+                if (sharedIndex1 > sharedIndex2) {
                     polygon.splice(sharedIndex1, 0, nonSharedVertex);
+                } else {
+                    polygon.splice(sharedIndex2, 0, nonSharedVertex);
                 }
-        
                 // Remove this triangle from the list
                 triangles.splice(i, 1);
         
@@ -289,6 +354,7 @@ function buildPolygons(connectedComponents) {
 function drawPolygons(polygons) {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+    let colorIndex = 0;
 
     const w = canvas.width;
     const h = canvas.height;
@@ -300,17 +366,31 @@ function drawPolygons(polygons) {
             context.lineTo(polygon[i].x * w, (1 - polygon[i].y) * h);
         }
         context.closePath();
-        context.strokeStyle = 'white';
+        context.strokeStyle = colors[colorIndex++ % colors.length];
         context.stroke();
     }
 }
 
-function clusterize(triangles) {
+function updateStatus(message) {
+        // Update the content
+        document.querySelector('#info').innerHTML = message;
+    
+        // Pause execution and allow the browser to redraw
+        return new Promise(resolve => setTimeout(resolve, 0));
+}
+
+async function clusterize(triangles) {
+    await updateStatus(`Indexing...`);
     const vertexToTrianglesMap = createVerticesToTrianglesMap(triangles);
+    await updateStatus(`Adjacencies...`);
     const adjacencyMap = createAdjacencyMap(vertexToTrianglesMap);
+    await updateStatus(`Clustering...`);
     const components = groupByAdjencyAndNormals(adjacencyMap);
+    await updateStatus(`Untriangulating...`);
     const polygons = buildPolygons(components);
+    await updateStatus(`Drawing...`);
     drawUvsOnTexture(textures, polygons);
+    await updateStatus(``);
 }
 
 
@@ -322,31 +402,37 @@ function drawUvsOnTexture(textures, polygons) {
         const canvas = document.getElementById('canvas');
         const context = canvas.getContext('2d');
         
-        canvas.width = texture.image.width;
-        canvas.height = texture.image.height;
+        canvas.width = texture.image.width * scale;
+        canvas.height = texture.image.height * scale;
         
         context.drawImage(texture.image, 0, 0, canvas.width, canvas.height);
         
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        //const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
         
         // draw the uv triangles
-        for (let i = 0; i < textures.length; i++) {
-            const uvA = textures[i][0];
-            const uvB = textures[i][1];
-            const uvC = textures[i][2];
-        
-            const a = new THREE.Vector2(uvA.x * canvas.width, (1 - uvA.y) * canvas.height);
-            const b = new THREE.Vector2(uvB.x * canvas.width, (1 - uvB.y) * canvas.height);
-            const c = new THREE.Vector2(uvC.x * canvas.width, (1 - uvC.y) * canvas.height);
-        
-            context.beginPath();
-            context.moveTo(a.x, a.y);
-            context.lineTo(b.x, b.y);
-            context.lineTo(c.x, c.y);
-            context.closePath();
-            context.strokeStyle = 'red';
-            context.stroke();
+        if (showtriangles) {
+            for (let i = 0; i < textures.length; i++) {
+                const uvA = textures[i][0];
+                const uvB = textures[i][1];
+                const uvC = textures[i][2];
+            
+                const a = new THREE.Vector2(uvA.x * canvas.width, (1 - uvA.y) * canvas.height);
+                const b = new THREE.Vector2(uvB.x * canvas.width, (1 - uvB.y) * canvas.height);
+                const c = new THREE.Vector2(uvC.x * canvas.width, (1 - uvC.y) * canvas.height);
+            
+                context.beginPath();
+                context.moveTo(a.x, a.y);
+                context.lineTo(b.x, b.y);
+                context.lineTo(c.x, c.y);
+                context.closePath();
+                context.strokeStyle = '#C0C0C0';
+                context.stroke();
+            }
         }
-        drawPolygons(polygons);
+        if (showpolygons) {
+            drawPolygons(polygons);
+        }
     });
 }
+
+drawPage()
